6e333ca7a1509496a30948b28ce03f98af23f802
anon <vikram.saraph.22@gmail.com>
2022-08-31 22:21:25 -0400

    update react code

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+// *really* simple example of browser client subscribing the reddit comments via websocket
+// this is how a server can push state to a web client
+// things like push notification etc
+
+// use create-react-app to create a React application
+// replace src/App.js with the contents of this file
+// then run the app with `npm start` and `ws_server.py` running
+// and open a web console. You should see reddit comments printed live in the console
+
+// ripped and modified from here: https://stackoverflow.com/a/60161181
+
 import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isPaused, setPause] = useState(false);
+    const ws = useRef(null);
+
+    useEffect(() => {
+        ws.current = new WebSocket("ws://localhost:8765");
+        ws.current.onopen = () => console.log("ws opened");
+        ws.current.onclose = () => console.log("ws closed");
+
+        const wsCurrent = ws.current;
+
+        return () => {
+            wsCurrent.close();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (!ws.current) return;
+
+        ws.current.onmessage = e => {
+            if (isPaused) return;
+            const message = e.data;
+            console.log(message);
+        };
+    }, [isPaused]);
+
+    return (
+        <div>
+            <button onClick={() => setPause(!isPaused)}>
+                {isPaused ? "Resume" : "Pause"}
+            </button>
+        </div>
+    );
 }
+
 
 export default App;
